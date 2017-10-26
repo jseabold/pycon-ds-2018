360630c49d3488b89ec4b130d653b2a51092dfbb
Skipper Seabold <sseabold@civisanalytics.com>
2017-10-26 14:09:15 -0500

    Add environment and check

---
+++ imports-test.py
@@ -0,0 +1,8 @@
+import requests
+import numpy as np
+from scipy import linalg
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from sklearn.preprocessing import Normalizer
+print("All imports succeeded.")
