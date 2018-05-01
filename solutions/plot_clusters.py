bb53880c144d0b6380a1e3fd787002b798f338e1
Skipper Seabold <sseabold@civisanalytics.com>
2018-05-01 11:32:00 -0400

    Cleanup and typo fixes

--- solutions/plot_clusters.py
+++ solutions/plot_clusters.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 fig, axes = plt.subplots(nrows=2, figsize=(12, 10))
 
 axes[0].hist(kmeans.labels_, bins=n_clusters)
-axes[1].hist(dta.violations.str.extract("(\d+)(?=\.)", expand=False).astype(int), bins=n_clusters);
+axes[1].hist((dta.violations.str.extract("(\d+)(?=\.)", expand=False)
+              .astype(int)
+              .rank(method='dense')), bins=n_clusters);
 
-dta.violations[kmeans.labels_ == 0].iloc[:5].values
-dta.violations[kmeans.labels_ == 1].iloc[:5].values
-dta.violations[kmeans.labels_ == 2].iloc[:5].values
+# dta.violations[kmeans.labels_ == 0].iloc[:5].values
+# dta.violations[kmeans.labels_ == 1].iloc[:5].values
+# dta.violations[kmeans.labels_ == 2].iloc[:5].values
